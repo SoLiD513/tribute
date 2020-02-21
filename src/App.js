c2a8c83d2b51512515316645441d9c2d1e92fbe8
Jesse McKinney <jessman51386@gmail.com>
2020-02-21 15:53:07 -0500

    yay

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import './App.css';
-import Nav from './Nav'
-import Hero from './Hero'
-import Stats from './Stats'
-import Gallery from './Gallery'
-import Event from './Event'
-import Bio from './Bio'
-import Comments from './Comments';
+import React from "react";
+import "./App.css";
+import Nav from "./Nav";
+import Hero from "./Hero";
+import Stats from './Stats';
+import Gallery from './Gallery';
+import Event from './Event';
+import Bio from './Bio';
+import Comments from "./Comments";
 
 function App() {
-  
   return (
     <div className="App">
-      <div id='main'>
-        <Nav />
-        <Hero />
-        <Stats />
-        <Gallery />
-        <Event />
-        <Bio />
+      <div id="main">
+      <Nav />
+      <Hero />
+      <Stats />
+      <Gallery />
+      <Event />
+      <Bio />
         <Comments />
       </div>
     </div>
